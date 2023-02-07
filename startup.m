27437fa033e1360698c382cafece223d4b4eb841
mrahfa <mrahfa@gmail.com>
2023-02-07 12:37:31 +0800

    Update

--- startup.m
+++ startup.m
@@ -1,26 +1,26 @@
 % Housekeeping Program to Create Storage Folder
-% 
+%
 % MATLAB version: starting with R2018b
 %
 % Revisions:
 % -------
+% 2023.02.07 - Function update
 % 2019.03.24 - Function creation
-% 
+%
 % Created by: Ahmad Faisal Ayob / ahmad.faisal@umt.edu.my
 
-if ~exist('data', 'dir')
-    mkdir('data')
-    cd data
- 
-    mkdir('Forward')
-    mkdir('Left')
-    mkdir('Right')
-    
-    mkdir('Forward_processed')
-    mkdir('Left_processed')
-    mkdir('Right_processed')
-    cd ..
-end
-addpath('actions')
+
+mkdir('data')
+cd data
+
+mkdir('Forward')
+mkdir('Left')
+mkdir('Right')
+
+mkdir('Forward_processed')
+mkdir('Left_processed')
+mkdir('Right_processed')
+cd ..
+
 addpath('helpers')
-addpath('data')
+
